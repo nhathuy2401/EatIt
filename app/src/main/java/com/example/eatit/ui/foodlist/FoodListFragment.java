a30a96f0812afdefe98d6f498b78beeb8457e2cc
nhathuy2401 <35965657+nhathuy2401@users.noreply.github.com>
2020-09-02 18:47:20 +0700

    hello Push

--- app/src/main/java/com/example/eatit/ui/foodlist/FoodListFragment.java
+++ app/src/main/java/com/example/eatit/ui/foodlist/FoodListFragment.java
@@ -60,7 +60,7 @@ public class FoodListFragment extends Fragment {
     }
 
     private void initView() {
-//ad
+//adđ
             ((AppCompatActivity)getActivity())
                     .getSupportActionBar()
                     .setTitle( Common.categorySelected.getName() );
